aa969df1dfdc090ea134d63d52ed9d3ea5aab556
ivogiake <259051186+JohnStarlight@users.noreply.github.com>
2026-02-07 13:31:30 +0200

    Initial commit

---
+++ quadA.go
@@ -0,0 +1,7 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func QuadA(x, y int) {
+	z01.PrintRune('a')
+}
---
+++ quadC.go
@@ -0,0 +1,7 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func QuadC(x, y int) {
+	z01.PrintRune('a')
+}
