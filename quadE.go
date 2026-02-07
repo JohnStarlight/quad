0cb79d012324e9c661f8b768da06bbdec32683f1
Ioannis Vogiakelis <259051186+JohnStarlight@users.noreply.github.com>
2026-02-07 18:14:00 +0000

    fixed

--- quadE.go
+++ quadE.go
@@ -2,28 +2,18 @@ package piscine
 
 import "github.com/01-edu/z01"
 
-func printLBifLastE(i, x int) {
-	if i == x-1 {
-		z01.PrintRune('\n')
-	}
-}
-
 func printIfMiddleLinesE(i, x int) {
 	if i == 0 || i == x-1 {
 		z01.PrintRune('B')
-		// check if last letter if true \n
-		printLBifLastE(i, x)
 	} else {
 		z01.PrintRune(' ')
 	}
 }
 
 func printIfFirstAndLastLinesE(j, i, y, x int) {
-	  if (j == 0 && i == 0)||(j == y-1 && i == x-1) {
+	  if (j==0 && i==0)||(j==y-1 && i==x-1) {
 			z01.PrintRune('A')
-			// check if last letter if true \n
-			printLBifLastE(i, x)
-	} else if (j == 0 && i == x-1)||(j == y-1 && i == 0) {
+	} else if (j==0 && i==x-1)||(j==y-1 && i==0) {
 			z01.PrintRune('C')
 	} else if (j==0 && i>0 && i<x-1) || (j==y-1 && i>0 && i<x-1) {
 			z01.PrintRune('B')
@@ -34,12 +24,12 @@ func printIfFirstAndLastLinesE(j, i, y, x int) {
 
 func QuadE(x, y int) {
 	// check if input is null
-	// if x <= 0 || y <= 0 {
-	// 	return 0
-	// }
-	for j := 0; j < y; j++ {
+	if x <= 0 || y <= 0 {
+		return
+}	for j := 0; j < y; j++ {
 		for i := 0; i < x; i++ {
 			printIfFirstAndLastLinesE(j, i, y, x)
 		}
+		z01.PrintRune('\n')
 	}
 }
