d910cebaed44e1a515ec47dfc0955fa1331e100e
skourou <skourou@invalid>
2026-02-07 15:45:15 +0200

    quad A with functions

--- quadA.go
+++ quadA.go
@@ -2,6 +2,44 @@ package piscine
 
 import "github.com/01-edu/z01"
 
+func printLBifLast(i, x int) {
+	if i == x-1 {
+		z01.PrintRune('\n')
+	}
+}
+
+func printIfMiddleLines(i, x int) {
+	if i == 0 || i == x-1 {
+		z01.PrintRune('|')
+		// check if last letter if true \n
+		printLBifLast(i, x)
+	} else {
+		z01.PrintRune(' ')
+	}
+}
+
+func printIfFirstAndLastLines(j, i, y, x int) {
+	if j == 0 || j == y-1 {
+		if i == 0 || i == x-1 {
+			z01.PrintRune('o')
+			// check if last letter if true \n
+			printLBifLast(i, x)
+		} else {
+			z01.PrintRune('-')
+		}
+	} else {
+		printIfMiddleLines(i, x)
+	}
+}
+
 func QuadA(x, y int) {
-	z01.PrintRune('a')
+	// check if input is null
+	// if x <= 0 || y <= 0 {
+	// 	return 0
+	// }
+	for j := 0; j < y; j++ {
+		for i := 0; i < x; i++ {
+			printIfFirstAndLastLines(j, i, y, x)
+		}
+	}
 }
---
+++ test_file.go
@@ -0,0 +1,49 @@
+// package main
+
+// import "github.com/01-edu/z01"
+
+// func printLBifLast(index, xpos int) {
+// 	if index == xpos-1 {
+// 		z01.PrintRune('\n')
+// 	}
+// }
+
+// func printIfMiddleLines(i, x int) {
+// 	if i == 0 || i == x-1 {
+// 		z01.PrintRune('|')
+// 		// check if last letter if true \n
+// 		printLBifLast(i, x)
+// 	} else {
+// 		z01.PrintRune(' ')
+// 	}
+// }
+
+// func printIfFirstAndLastLines(j, i, y, x int) {
+// 	if j == 0 || j == y-1 {
+// 		if i == 0 || i == x-1 {
+// 			z01.PrintRune('o')
+// 			// check if last letter if true \n
+// 			printLBifLast(i, x)
+// 		} else {
+// 			z01.PrintRune('-')
+// 		}
+// 	} else {
+// 		printIfMiddleLines(i, x)
+// 	}
+// }
+
+// func QuadAA(x, y int) {
+// 	// check if input is null
+// 	// if x <= 0 || y <= 0 {
+// 	// 	return 0
+// 	// }
+// 	for j := 0; j < y; j++ {
+// 		for i := 0; i < x; i++ {
+// 			printIfFirstAndLastLines(j, i, y, x)
+// 		}
+// 	}
+// }
+
+func main() {
+	QuadAA(7, 5)
+}
